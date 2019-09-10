d904ca57e7ae6ff49ed64649dee5bb38ccd38ef1
Matyas Perendi <perendi.matyas98@hotmail.com>
2019-09-10 21:56:53 +0200

    Constraints added, CPU and user win streak counters added

--- RockPaperScissors/ViewController.swift
+++ RockPaperScissors/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //User's win counter
+    var user_win_count = 0
+    
+    //CPU win counter
+    var cpu_win_count = 0
+    
     //Possible choices the computer can make
     var computersValues = ["Rock","Paper","Scissors"]
     
@@ -30,38 +37,58 @@ class ViewController: UIViewController {
         }
         else if usersChoice == "Rock" && computersChoice == "Scissors" {
             result.text = "Computer's choice: \(computersChoice) \n Your choice: \(usersChoice) \n Congratulations, you won!"
+            user_win_count += 1
+            cpu_win_count = 0
+            
             
         }
         else if usersChoice == "Paper" && computersChoice == "Rock"{
             result.text = "Computer's choice: \(computersChoice) \n Your choice: \(usersChoice) \n Congratulations, you won!"
+            user_win_count += 1
+            cpu_win_count = 0
             
         }
         else if usersChoice == "Scissors" && computersChoice == "Paper"{
             result.text = "Computer's choice: \(computersChoice) \n Your choice: \(usersChoice) \n Congratulations, you won!"
+            user_win_count += 1
+            cpu_win_count = 0
             
         }
         else {
             result.text = "Computer's choice: \(computersChoice) \n Your choice: \(usersChoice) \n You lost, the Computer won this time!"
+            user_win_count = 0
+            cpu_win_count += 1
             
         }
+    }
+    
+    func refreshWinStreak() {
+        user_win_counter.text = user_win_count.description
+        cpu_win_counter.text = cpu_win_count.description
     }
     
     //Rock button
     @IBAction func rockClicked(_ sender: UIButton) {
         getResult(usersChoice: "Rock")
+        refreshWinStreak()
     }
     //Scissors button
     @IBAction func scissorsClicked(_ sender: UIButton) {
         getResult(usersChoice: "Scissors")
+        refreshWinStreak()
     }
     //Paper button
     @IBAction func paperClicked(_ sender: UIButton) {
         getResult(usersChoice: "Paper")
+        refreshWinStreak()
     }
+    //User win counter
+    @IBOutlet weak var user_win_counter: UILabel!
     //Text area
     @IBOutlet weak var result: UITextView!
     
-    }
+    @IBOutlet weak var cpu_win_counter: UILabel!
+}
 
     
     
